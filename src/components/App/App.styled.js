78999566b247231bb186d7d50ed0d4e29340512c
MateichukNina <124276133+MateichukNina@users.noreply.github.com>
2023-10-02 00:26:33 +0300

    correct final

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -3,18 +3,12 @@ import styled from 'styled-components';
 
 export const AppWrapper = styled.div`
 
-  // display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 20px;
    width: 100%;
    height: 100vh;
   margin-right: auto;
-  margin-left: auto;
+  justify-content: flex-end;
   padding: 10px;
-  margin-top: 25px;
-  border-radius: 8px;
-  // background-color: rgb(153, 255, 153);
+ 
   background-image: linear-gradient(to bottom right, rgb(153, 255, 153), #000ff0);
   box-shadow: 1px 1px 4px 0px rgba(0, 0, 0, 0.5);
 `;
